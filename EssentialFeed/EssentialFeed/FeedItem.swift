23bd16a3eb39cbc71fd62aa1b04b4534d4ca2cae
Christophe Bugnon <cbugnon@protonmail.com>
2020-10-07 13:55:54 +0200

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Christophe Bugnon on 07/10/2020.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

cdc074b739843bcedf895f3a8a9230c4eb4edc62
Christophe Bugnon <cbugnon@protonmail.com>
2021-02-22 09:00:11 +0100

    Move `FeedItemsMapper` to its own file

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -34,36 +34,3 @@ public final class RemoteFeedLoader {
         }
     }
 }
-
-private class FeedItemsMapper {
-    private struct Root: Decodable {
-        private let items: [Item]
-        var feed: [FeedItem] {
-            return items.map { $0.feed }
-        }
-    }
-    
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var feed: FeedItem {
-            return FeedItem(id: id,
-                            description: description,
-                            location: location,
-                            imageURL: image)
-        }
-    }
-    
-    private static var OK_200: Int { return 200 }
-
-    static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard let root = try? JSONDecoder().decode(Root.self, from: data),
-              response.statusCode == OK_200 else {
-            return .failure(.invalidData)
-        }
-        return .success(root.feed)
-    }
-}
